0ec8f1ab6abc0a5b41d92cc233e37a8115b4a8bd
dougiesquire <dougiesquire@gmail.com>
2022-11-23 15:08:40 +1100

    Build cice6

--- build/build.sh
+++ build/build.sh
@@ -18,57 +18,65 @@ export NETCDF_PATH=/apps/netcdf/4.7.3
 export PKG_CONFIG_PATH=/apps/netcdf/4.7.3/lib/pkgconfig:/apps/intel-ct/2022.1.0/mkl/lib/pkgconfig:/half-root/usr/lib64/pkgconfig:/apps/netcdf/4.7.3/lib/Intel/pkgconfig:/apps/netcdf/4.7.3/lib/Intel/pkgconfig
 export ESMFMKFILE=$esmf_dir/lib/libg/Linux.intel.x86_64_medium.openmpi.default/esmf.mk
 
-#cmeps_dir=$cesm_dir/components/cmeps
-#A_bld_dir=/scratch/tm70/ds0092/cime/scratch/A/bld
-#GMOM_bld_dir=/scratch/tm70/ds0092/cime/scratch/GMOM_JRA_WD/bld
-# A_bld_dir=$GMOM_bld_dir
-#nuopc_bld_dir=$GMOM_bld_dir/intel/openmpi/nodebug/nothreads/nuopc
-
 cwd=$(pwd)
 
 cesm_dir=/g/data/tm70/ds0092/CESM
-sharedlib_root=/scratch/tm70/ds0092/cime/scratch/GMOM_JRA_WD/bld/intel/openmpi/nodebug/nothreads/nuopc
+bld_dir=/scratch/tm70/ds0092/cime/scratch/D_JRA_WD/bld
+sharedlib_dir=${bld_dir}/intel/openmpi/nodebug/nothreads/nuopc
 
 # Build CDEPS
 # ====================
-cdeps_bld_dir=${cwd}/cdeps
-mkdir -p $cdeps_bld_dir
-cd $cdeps_bld_dir
-cmake -DSRC_ROOT=${cesm_dir}  -Dcompile_threaded="FALSE"  -DCASEROOT=${cwd} -DCIMEROOT="${cesm_dir}/cime" -DCOMPILER="intel" -DDEBUG="FALSE" -DMACH="gadi" -DMPILIB="openmpi" -DNINST_VALUE="c1a1i1o1r1w1" -DOS="LINUX" -DPIO_VERSION="2" -DCMAKE_Fortran_COMPILER_WORKS=1 -DCMAKE_INSTALL_PREFIX:PATH=/ -DLIBROOT=${sharedlib_root}  -DPIO_C_LIBRARY=${sharedlib_root}/lib -DPIO_C_INCLUDE_DIR=${sharedlib_root}/include  -DPIO_Fortran_LIBRARY=${sharedlib_root}/lib -DPIO_Fortran_INCLUDE_DIR=${sharedlib_root}/include $cesm_dir/components/cdeps
-make install VERBOSE=1 DESTDIR="./"
-cd ${cwd}
+# cdeps_bld_dir=${cwd}/cdeps
+# mkdir -p $cdeps_bld_dir
+# cd $cdeps_bld_dir
+# cmake -DSRC_ROOT=${cesm_dir}  -Dcompile_threaded="FALSE"  -DCASEROOT=${cwd} -DCIMEROOT="${cesm_dir}/cime" -DCOMPILER="intel" -DDEBUG="FALSE" -DMACH="gadi" -DMPILIB="openmpi" -DNINST_VALUE="c1a1i1o1r1w1" -DOS="LINUX" -DPIO_VERSION="2" -DCMAKE_Fortran_COMPILER_WORKS=1 -DCMAKE_INSTALL_PREFIX:PATH=/ -DLIBROOT=${sharedlib_root}  -DPIO_C_LIBRARY=${sharedlib_root}/lib -DPIO_C_INCLUDE_DIR=${sharedlib_root}/include  -DPIO_Fortran_LIBRARY=${sharedlib_root}/lib -DPIO_Fortran_INCLUDE_DIR=${sharedlib_root}/include $cesm_dir/components/cdeps
+# make install VERBOSE=1 DESTDIR="./"
+# cd ${cwd}
 
-mkdir -p ${cwd}/lib
+# mkdir -p ${cwd}/lib
 
 # Build atm data model
 # ====================
-cd ${cdeps_bld_dir}/datm
+cd ${bld_dir}/atm/obj
 make datm
-ln -sf ${cdeps_bld_dir}/datm/libdatm.a ${cwd}/lib/libatm.a
+ln -sf ${bld_dir}/atm/obj/libdatm.a ${bld_dir}/lib/libatm.a
 cd ${cwd}
 
 # Build rof data model
 # ====================
-cd ${cdeps_bld_dir}/drof
+cd ${bld_dir}/rof/obj
 make drof
-ln -sf ${cdeps_bld_dir}/drof/libdrof.a ${cwd}/lib/librof.a
+ln -sf ${bld_dir}/rof/obj/libdrof.a ${bld_dir}/lib/librof.a
 cd ${cwd}
 
 # Build ocn model
 # ====================
 if [ "$ACTIVE_OCN" = true ] ; then
-    echo "Not implemented"
+    echo "Not implemented" && exit
 else
-    cd ${cdeps_bld_dir}/docn
+    if [ -L ${bld_dir}"/ocn/obj" ]; then
+        unlink ${bld_dir}"/ocn/obj"
+    else
+        rm -r ${bld_dir}"/ocn/obj"
+    fi
+    ln -sf ${sharedlib_dir}/CDEPS/docn ${bld_dir}/ocn/obj
+    cd ${bld_dir}/ocn/obj
     make docn
-    ln -sf ${cdeps_bld_dir}/docn/libdocn.a ${cwd}/lib/libocn.a
-    cd ${cwd}
+    ln -sf ${bld_dir}/ocn/obj/libdocn.a ${bld_dir}/lib/libocn.a
 fi
+cd ${cwd}
 
 # Build CICE6
 # ====================
-cd ${cwd}/cice6
-make complib -j 8 COMP_NAME=cice COMPLIB=${cwd}/lib/libice.a -f ${cesm_dir}/cime/CIME/Tools/Makefile USER_CPPDEFS=" -Dncdf" CIME_MODEL=cesm  SMP=FALSE CASEROOT=${cwd} CASETOOLS=${cesm_dir}/cime/CIME/Tools CIMEROOT=${cesm_dir}/cime SRCROOT=${cesm_dir} COMP_INTERFACE="nuopc" COMPILER="intel" DEBUG="FALSE" EXEROOT="/scratch/tm70/ds0092/cime/scratch/GMOM_JRA_WD/bld" RUNDIR="/scratch/tm70/ds0092/cime/scratch/GMOM_JRA_WD/run" INCROOT="/scratch/tm70/ds0092/cime/scratch/GMOM_JRA_WD/bld/lib/include" LIBROOT="/scratch/tm70/ds0092/cime/scratch/GMOM_JRA_WD/bld/lib" MACH="gadi" MPILIB="openmpi" NINST_VALUE="c1a1i1o1r1w1" OS="LINUX" PIO_VERSION=2 SHAREDLIBROOT="/scratch/tm70/ds0092/cime/scratch/GMOM_JRA_WD/bld" SMP_PRESENT="FALSE" USE_ESMF_LIB="TRUE" USE_MOAB="FALSE" COMP_LND="slnd" USE_PETSC="FALSE"
+# Note, the generic CIME Makefile used here looks for xmlquery in 
+# CASEROOT. However, it only uses this to query what the atm
+# component is, which is not important to the ice build. So I think
+# we can safely set CASEROOT=${cwd} and copy other files looked for
+# CASEROOT to there (Depends.intel, Macros.make)
+cd ${bld_dir}/ice/obj
+cp ${cwd}/Filepath.cice6 ${bld_dir}/ice/obj/Filepath
+make complib -j 8 COMP_NAME=cice COMPLIB=${bld_dir}/lib/libice.a -f ${cesm_dir}/cime/CIME/Tools/Makefile USER_CPPDEFS=" -Dncdf" CIME_MODEL=cesm  SMP=FALSE CASEROOT=${cwd} CASETOOLS=${cesm_dir}/cime/CIME/Tools CIMEROOT=${cesm_dir}/cime SRCROOT=${cesm_dir} COMP_INTERFACE="nuopc" COMPILER="intel" DEBUG="FALSE" EXEROOT=${bld_dir} INCROOT="${bld_dir}/lib/include" LIBROOT="${bld_dir}/lib" MACH="gadi" MPILIB="openmpi" NINST_VALUE="c1a1i1o1r1w1" OS="LINUX" PIO_VERSION=2 SHAREDLIBROOT=${bld_dir} SMP_PRESENT="FALSE" USE_ESMF_LIB="TRUE" USE_MOAB="FALSE" COMP_LND="slnd" USE_PETSC="FALSE"
+cd ${cwd}
 
 # Build WW3
 # ====================
